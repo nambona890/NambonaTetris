44062e8a32dd8973a2b46de68a4e7deb71c10c59
nambona890 <zander.morrow@gmail.com>
2019-12-19 07:37:26 +1100

    Put everything into one file because shit kept breaking

--- NambonaTetris/Main.c
+++ NambonaTetris/Main.c
@@ -7,6 +7,24 @@ char(*currentPiecePtr)[4][4][2];
 char pieceRotation;
 char pieceX;
 char pieceY;
+const short width;
+const short height;
+const char mapWidth;
+const char mapHeight;
+const char tileSize;
+const short screenOffsetX;
+const short screenOffsetY;
+float deltaTime;
+bool keyPress[6];
+bool keyHeld[6];
+char blockGrid[21][10];
+const char iPiece[4][4][2];
+const char sPiece[4][4][2];
+const char jPiece[4][4][2];
+const char zPiece[4][4][2];
+const char tPiece[4][4][2];
+const char oPiece[4][4][2];
+const char lPiece[4][4][2];
 
 int main(int argc, char **argv)
 {
@@ -125,4 +143,411 @@ int SetPress(char key)
 		keyPress[key] == 0;
 	}
 	keyHeld[key] == 1;
+}
+
+
+
+
+
+
+int LoadImages(SDL_Renderer* renderer)
+{
+	pieceTile = IMG_LoadTexture(renderer, "Images/Piece.png");
+	pieceTileSrc.x = 0;
+	pieceTileSrc.y = 0;
+	pieceTileSrc.w = tileSize;
+	pieceTileSrc.h = tileSize;
+}
+
+int ScreenDraw(SDL_Renderer* renderer, SDL_Texture* canvas)
+{
+	SDL_SetRenderTarget(renderer, canvas);
+	SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+	SDL_RenderClear(renderer);
+	SDL_SetRenderDrawColor(renderer, 255, 192, 192, 192);
+
+	for (int i = 0; i < 20; i++)
+	{
+		for (int j = 0; j < 10; j++)
+		{
+			if (blockGrid[i][j] != PIECE_NONE)
+			{
+				SDL_Rect pieceDest;
+				pieceDest.w = tileSize;
+				pieceDest.h = tileSize;
+
+				pieceDest.x = (j * tileSize) + screenOffsetX;
+				pieceDest.y = (j * tileSize) + screenOffsetY;
+
+				SDL_RenderCopy(renderer, pieceTile, &pieceTileSrc, &pieceDest);
+			}
+		}
+	}
+	SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+
+	for (int i = 0; i < 4; i++)
+	{
+		SDL_Rect pieceDest;
+		pieceDest.w = tileSize;
+		pieceDest.h = tileSize;
+
+		pieceDest.x = (((char)&currentPiecePtr[pieceRotation][i][0] + pieceX) * tileSize) + screenOffsetX;
+		pieceDest.y = (((char)&currentPiecePtr[pieceRotation][i][1] + pieceY) * tileSize) + screenOffsetY;
+	}
+
+	return 0;
+}
+
+int ScreenRender(SDL_Renderer* renderer, SDL_Texture* canvas)
+{
+	SDL_SetRenderTarget(renderer, NULL);
+	SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+	SDL_RenderClear(renderer);
+	SDL_RenderCopy(renderer, canvas, NULL, NULL);
+	SDL_RenderPresent(renderer);
+	return 0;
+}
+
+
+
+
+
+
+
+int PieceInit()
+{
+	time_t t;
+	srand((unsigned)time(&t));
+	nextPiece = rand() % 7;
+	PieceDrop();
+}
+
+int PieceDrop()
+{
+	currentPiece = nextPiece;
+	nextPiece = rand() % 7;
+	pieceRotation = 0;
+	pieceX = 5;
+	pieceY = 0;
+	switch (currentPiece)
+	{
+	case PIECE_I:
+		currentPiecePtr = &iPiece;
+		break;
+	case PIECE_S:
+		currentPiecePtr = &sPiece;
+		break;
+	case PIECE_J:
+		currentPiecePtr = &jPiece;
+		break;
+	case PIECE_Z:
+		currentPiecePtr = &zPiece;
+		break;
+	case PIECE_T:
+		currentPiecePtr = &tPiece;
+		break;
+	case PIECE_O:
+		currentPiecePtr = &oPiece;
+		break;
+	case PIECE_L:
+		currentPiecePtr = &lPiece;
+		break;
+	default:
+		currentPiecePtr = NULL;
+		break;
+	}
+}
+
+int PieceWait()
+{
+	fallTimer += deltaTime;
+	if (fallTimer > fallWait)
+	{
+		fallTimer = 0;
+		for (int i = 0; i < 4; i++)
+		{
+			char curX = *currentPiecePtr[pieceRotation][i][0] + pieceX;
+			char curY = *currentPiecePtr[pieceRotation][i][1] + pieceY;
+
+			if (blockGrid[curY + 1][curX] != PIECE_NONE || curY > 18)
+			{
+				goto PieceWait_For_Break1;
+			}
+		}
+		pieceY++;
+		goto PieceWait_If_Break;
+	PieceWait_For_Break1:
+		for (int i = 0; i < 4; i++)
+		{
+			char curX = *currentPiecePtr[pieceRotation][i][0] + pieceX;
+			char curY = *currentPiecePtr[pieceRotation][i][1] + pieceY;
+
+			blockGrid[curY][curX] = currentPiece;
+		}
+		for (int i = 0; i < 20; i++)
+		{
+			bool clear = true;
+			for (int j = 0; j < 10; j++)
+			{
+				if (blockGrid[i][j] == PIECE_NONE)
+				{
+					clear = false;
+				}
+			}
+			if (clear)
+			{
+				for (int j = 0; j < 10; j++)
+				{
+					blockGrid[0][j] = PIECE_NONE;
+				}
+				if (i > 0)
+				{
+					for (int j = i; j >= 0; j++)
+					{
+						for (int k = 0; k < 10; k++)
+						{
+							blockGrid[j][k] = blockGrid[j - 1][k];
+						}
+					}
+				}
+			}
+			PieceDrop();
+		}
+	}
+PieceWait_If_Break:
+	return;
+}
+
+int PieceRotate(bool cw)
+{
+	bool fail = false;
+	char pieceRotationTemp = pieceRotation;
+	char pieceXTemp = pieceX;
+	char pieceYTemp = pieceY;
+	if (cw)
+	{
+		if (pieceRotation < 3)
+		{
+			pieceRotation++;
+		}
+		else
+		{
+			pieceRotation = 0;
+		}
+	}
+	else
+	{
+		if (pieceRotation > 0)
+		{
+			pieceRotation--;
+		}
+		else
+		{
+			pieceRotation = 3;
+		}
+	}
+	for (int i = 0; i < 4; i++)
+	{
+		for (int j = 0; j < 4; j++)
+		{
+			char curX = *currentPiecePtr[pieceRotation][j][0] + pieceX;
+			char curY = *currentPiecePtr[pieceRotation][j][1] + pieceY;
+			if (blockGrid[curY][curX] != PIECE_NONE || pieceX < 0)
+			{
+				pieceX++;
+			}
+		}
+	}
+	for (int i = 0; i < 4; i++)
+	{
+		char curX = *currentPiecePtr[pieceRotation][i][0] + pieceX;
+		char curY = *currentPiecePtr[pieceRotation][i][1] + pieceY;
+		if (blockGrid[curY][curX] != PIECE_NONE || pieceX < 0)
+		{
+			fail = true;
+		}
+	}
+	if (!fail)
+	{
+		return;
+	}
+	fail = false;
+	for (int i = 0; i < 4; i++)
+	{
+		for (int j = 0; j < 4; j++)
+		{
+			char curX = *currentPiecePtr[pieceRotation][j][0] + pieceX;
+			char curY = *currentPiecePtr[pieceRotation][j][1] + pieceY;
+			if (blockGrid[curY][curX] != PIECE_NONE || pieceX > 9)
+			{
+				pieceX--;
+			}
+		}
+	}
+	for (int i = 0; i < 4; i++)
+	{
+		char curX = *currentPiecePtr[pieceRotation][i][0] + pieceX;
+		char curY = *currentPiecePtr[pieceRotation][i][1] + pieceY;
+		if (blockGrid[curY][curX] != PIECE_NONE || pieceX > 9)
+		{
+			fail = true;
+		}
+	}
+	if (!fail)
+	{
+		return;
+	}
+	fail = false;
+	for (int i = 0; i < 4; i++)
+	{
+		for (int j = 0; j < 4; j++)
+		{
+			char curX = *currentPiecePtr[pieceRotation][j][0] + pieceX;
+			char curY = *currentPiecePtr[pieceRotation][j][1] + pieceY;
+			if (blockGrid[curY][curX] != PIECE_NONE)
+			{
+				pieceY++;
+			}
+		}
+	}
+	for (int i = 0; i < 4; i++)
+	{
+		char curX = *currentPiecePtr[pieceRotation][i][0] + pieceX;
+		char curY = *currentPiecePtr[pieceRotation][i][1] + pieceY;
+		if (blockGrid[curY][curX] != PIECE_NONE)
+		{
+			fail = true;
+		}
+	}
+	if (!fail)
+	{
+		return;
+	}
+	fail = false;
+	for (int i = 0; i < 4; i++)
+	{
+		for (int j = 0; j < 4; j++)
+		{
+			char curX = *currentPiecePtr[pieceRotation][j][0] + pieceX;
+			char curY = *currentPiecePtr[pieceRotation][j][1] + pieceY;
+			if (blockGrid[curY][curX] != PIECE_NONE || curY > 19)
+			{
+				pieceY--;
+			}
+		}
+	}
+	for (int i = 0; i < 4; i++)
+	{
+		char curX = *currentPiecePtr[pieceRotation][i][0] + pieceX;
+		char curY = *currentPiecePtr[pieceRotation][i][1] + pieceY;
+		if (blockGrid[curY][curX] != PIECE_NONE || curY > 19)
+		{
+			fail = true;
+		}
+	}
+	if (!fail)
+	{
+		return;
+	}
+
+	pieceRotation = pieceRotationTemp;
+	pieceX = pieceXTemp;
+	pieceY = pieceYTemp;
+}
+
+int PieceMove(bool dir)
+{
+	bool fail = false;
+	char pieceXTemp = pieceX;
+	char pieceYTemp = pieceY;
+
+	if (dir)
+	{
+		pieceX++;
+	}
+	else
+	{
+		pieceX--;
+	}
+
+	for (int i = 0; i < 4; i++)
+	{
+		for (int j = 0; j < 4; j++)
+		{
+			char curX = *currentPiecePtr[pieceRotation][j][0] + pieceX;
+			char curY = *currentPiecePtr[pieceRotation][j][1] + pieceY;
+			if (blockGrid[curY][curX] != PIECE_NONE || pieceX < 0)
+			{
+				pieceX++;
+			}
+		}
+	}
+	for (int i = 0; i < 4; i++)
+	{
+		char curX = *currentPiecePtr[pieceRotation][i][0] + pieceX;
+		char curY = *currentPiecePtr[pieceRotation][i][1] + pieceY;
+		if (blockGrid[curY][curX] != PIECE_NONE || pieceX < 0)
+		{
+			fail = true;
+		}
+	}
+	if (!fail)
+	{
+		return;
+	}
+	fail = false;
+	for (int i = 0; i < 4; i++)
+	{
+		for (int j = 0; j < 4; j++)
+		{
+			char curX = *currentPiecePtr[pieceRotation][j][0] + pieceX;
+			char curY = *currentPiecePtr[pieceRotation][j][1] + pieceY;
+			if (blockGrid[curY][curX] != PIECE_NONE || pieceX > 9)
+			{
+				pieceX--;
+			}
+		}
+	}
+	for (int i = 0; i < 4; i++)
+	{
+		char curX = *currentPiecePtr[pieceRotation][i][0] + pieceX;
+		char curY = *currentPiecePtr[pieceRotation][i][1] + pieceY;
+		if (blockGrid[curY][curX] != PIECE_NONE || pieceX > 9)
+		{
+			fail = true;
+		}
+	}
+	if (!fail)
+	{
+		return;
+	}
+	fail = false;
+	for (int i = 0; i < 4; i++)
+	{
+		for (int j = 0; j < 4; j++)
+		{
+			char curX = *currentPiecePtr[pieceRotation][j][0] + pieceX;
+			char curY = *currentPiecePtr[pieceRotation][j][1] + pieceY;
+			if (blockGrid[curY][curX] != PIECE_NONE)
+			{
+				pieceY++;
+			}
+		}
+	}
+	for (int i = 0; i < 4; i++)
+	{
+		char curX = *currentPiecePtr[pieceRotation][i][0] + pieceX;
+		char curY = *currentPiecePtr[pieceRotation][i][1] + pieceY;
+		if (blockGrid[curY][curX] != PIECE_NONE)
+		{
+			fail = true;
+		}
+	}
+	if (!fail)
+	{
+		return;
+	}
+
+	pieceX = pieceXTemp;
+	pieceY = pieceYTemp;
 }
--- NambonaTetris/Main.h
+++ NambonaTetris/Main.h
@@ -1,7 +1,291 @@
-#include <Global.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <SDL.h>
+#include <SDL_image.h>
+#include <stdbool.h>
+#include <math.h>
 
 #pragma once
 
+#define TAU (M_PI * 2)
+
+const short width = 1024;
+const short height = 768;
+
+const char mapWidth = 10;
+const char mapHeight = 20;
+
+const char tileSize = 32;
+
+const short screenOffsetX = 352;
+const short screenOffsetY = 40;
+
+float deltaTime = 0.03;
+
+SDL_Event event;
+
+char nextPiece;
+char currentPiece;
+char(*currentPiecePtr)[4][4][2];
+
+char pieceRotation;
+char pieceX;
+char pieceY;
+
+bool keyPress[6] = { 0,0,0,0,0,0 };
+bool keyHeld[6] = { 0,0,0,0,0,0 };
+
+char blockGrid[21][10] = {
+	{0,0,0,0,0,0,0,0,0,0},
+	{0,0,0,0,0,0,0,0,0,0},
+	{0,0,0,0,0,0,0,0,0,0},
+	{0,0,0,0,0,0,0,0,0,0},
+	{0,0,0,0,0,0,0,0,0,0},
+	{0,0,0,0,0,0,0,0,0,0},
+	{0,0,0,0,0,0,0,0,0,0},
+	{0,0,0,0,0,0,0,0,0,0},
+	{0,0,0,0,0,0,0,0,0,0},
+	{0,0,0,0,0,0,0,0,0,0},
+	{0,0,0,0,0,0,0,0,0,0},
+	{0,0,0,0,0,0,0,0,0,0},
+	{0,0,0,0,0,0,0,0,0,0},
+	{0,0,0,0,0,0,0,0,0,0},
+	{0,0,0,0,0,0,0,0,0,0},
+	{0,0,0,0,0,0,0,0,0,0},
+	{0,0,0,0,0,0,0,0,0,0},
+	{0,0,0,0,0,0,0,0,0,0},
+	{0,0,0,0,0,0,0,0,0,0},
+	{0,0,0,0,0,0,0,0,0,0},
+	{0,0,0,0,0,0,0,0,0,0}
+};
+
+#define PIECE_NONE 0
+#define PIECE_I 1
+#define PIECE_S 2
+#define PIECE_J 3
+#define PIECE_Z 4
+#define PIECE_T 5
+#define PIECE_O 6
+#define PIECE_L 7
+
+
+const char iPiece[4][4][2] =
+{
+	{
+		{-2,0},
+		{-1,0},
+		{0,0},
+		{1,0}
+	},
+	{
+		{0,-2},
+		{0,-1},
+		{0,0},
+		{0,1}
+	},
+	{
+		{-2,0},
+		{-1,0},
+		{0,0},
+		{1,0}
+	},
+	{
+		{0,-2},
+		{0,-1},
+		{0,0},
+		{0,1}
+	}
+};
+
+const char sPiece[4][4][2] =
+{
+	{
+		{-1,1},
+		{0,1},
+		{0,0},
+		{1,0}
+	},
+	{
+		{0,-1},
+		{0,0},
+		{1,0},
+		{1,1}
+	},
+	{
+		{-1,1},
+		{0,1},
+		{0,0},
+		{1,0}
+	},
+	{
+		{0,-1},
+		{0,0},
+		{1,0},
+		{1,1}
+	}
+};
+
+const char jPiece[4][4][2] =
+{
+	{
+		{-1,0},
+		{0,0},
+		{1,0},
+		{1,1}
+	},
+	{
+		{-1,1},
+		{0,1},
+		{0,0},
+		{0,-1}
+	},
+	{
+		{-1,-1},
+		{-1,0},
+		{0,0},
+		{1,0}
+	},
+	{
+		{1,-1},
+		{0,-1},
+		{0,0},
+		{0,1}
+	}
+};
+
+const char zPiece[4][4][2] =
+{
+	{
+		{-1,0},
+		{0,0},
+		{0,1},
+		{1,1}
+	},
+	{
+		{0,1},
+		{0,0},
+		{1,0},
+		{1,-1}
+	},
+	{
+		{-1,0},
+		{0,0},
+		{0,1},
+		{1,1}
+	},
+	{
+		{0,1},
+		{0,0},
+		{1,0},
+		{1,-1}
+	}
+};
+
+extern const char tPiece[4][4][2] =
+{
+	{
+		{-1,0},
+		{0,0},
+		{1,0},
+		{0,1}
+	},
+	{
+		{0,-1},
+		{0,0},
+		{0,1},
+		{-1,0}
+	},
+	{
+		{-1,0},
+		{0,0},
+		{1,0},
+		{0,-1}
+	},
+	{
+		{0,-1},
+		{0,0},
+		{0,1},
+		{1,0}
+	}
+};
+
+const char oPiece[4][4][2] =
+{
+	{
+		{-1,0},
+		{0,0},
+		{-1,1},
+		{0,1}
+	},
+	{
+		{-1,0},
+		{0,0},
+		{-1,1},
+		{0,1}
+	},
+	{
+		{-1,0},
+		{0,0},
+		{-1,1},
+		{0,1}
+	},
+	{
+		{-1,0},
+		{0,0},
+		{-1,1},
+		{0,1}
+	}
+};
+
+const char lPiece[4][4][2] =
+{
+	{
+		{-1,1},
+		{-1,0},
+		{0,0},
+		{1,0}
+	},
+	{
+		{-1,-1},
+		{0,-1},
+		{0,0},
+		{0,1}
+	},
+	{
+		{-1,0},
+		{0,0},
+		{1,0},
+		{1,-1}
+	},
+	{
+		{0,-1},
+		{0,0},
+		{0,1},
+		{1,1}
+	}
+};
+
+SDL_Texture* pieceTile = NULL;
+SDL_Rect pieceTileSrc;
+
+float fallTimer = 0;
+float fallWait = 0.5;
+
+int PieceInit();
+
+int PieceDrop();
+
+int PieceWait();
+
+int PieceRotate(bool cw);
+
+int PieceMove(bool dir);
+
+int LoadImages(SDL_Renderer* renderer);
+
+int ScreenDraw(SDL_Renderer* renderer, SDL_Texture* canvas);
+
+int ScreenRender(SDL_Renderer* renderer, SDL_Texture* canvas);
 
 int mod(int a, int b);
 
